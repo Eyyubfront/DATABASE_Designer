cde9e3579743b42873fe64bfddcda8060c855898
Eyyubfront <eyyubbayramov58@gmail.com>
2025-11-27 14:28:56 +0400

    new

--- e-commerce_table-1764236152.sql
+++ e-commerce_table-1764236152.sql
@@ -1,65 +1,51 @@
-CREATE TABLE [Customers] (
-	[id] int IDENTITY(1,1) NOT NULL UNIQUE,
-	[Name] nvarchar(30) NOT NULL,
-	[Email] nvarchar(30) NOT NULL,
-	[Phone] nvarchar(30) NOT NULL,
-	PRIMARY KEY ([id])
+CREATE TABLE Customers (
+Id int primary key IDENTITY,
+Name nvarchar(30) NOT NULL,
+Email nvarchar(30) NOT NULL unique,
+Phone nvarchar(30) NOT NULL,
 );
-
-CREATE TABLE [Products] (
-	[id] int IDENTITY(1,1) NOT NULL UNIQUE,
-	[Name] nvarchar(40) NOT NULL,
-	[Description] nvarchar(20) NOT NULL,
-	[Category_Id] int NOT NULL,
-	PRIMARY KEY ([id])
-);
-
-CREATE TABLE [Categories] (
-	[id] int IDENTITY(1,1) NOT NULL UNIQUE,
-	[Name] nvarchar(40) NOT NULL,
-	[Description] nvarchar(20) NOT NULL,
-	PRIMARY KEY ([id])
-);
-
-CREATE TABLE [Orders] (
-	[id] int IDENTITY(1,1) NOT NULL UNIQUE,
-	[OrderDate] time(7) NOT NULL,
-	[Status] nvarchar(40) NOT NULL,
-	[Customer_Id] int NOT NULL,
-	PRIMARY KEY ([id])
-);
-
-CREATE TABLE [Suppliers] (
-	[id] int IDENTITY(1,1) NOT NULL UNIQUE,
-	[Name] nvarchar(50) NOT NULL,
-	[ContacInfo] nvarchar(60) NOT NULL,
-	PRIMARY KEY ([id])
-);
-
-CREATE TABLE [OrderItems] (
-	[id] int IDENTITY(1,1) NOT NULL UNIQUE,
-	[Quantity] int NOT NULL,
-	[UnitPrice] int NOT NULL,
-	[Orders_Id] int NOT NULL,
-	[Products_Id] int NOT NULL,
-	PRIMARY KEY ([id])
-);
-
-CREATE TABLE [ProductSuppliers] (
-	[id] int IDENTITY(1,1) NOT NULL UNIQUE,
-	[Products_Id] int NOT NULL,
-	[Suplier_Id] int NOT NULL,
-	PRIMARY KEY ([id])
+CREATE TABLE Orders (
+	Id int primary key IDENTITY,
+	OrderDate time(7) NOT NULL,
+	Status varchar(40) NOT NULL,
+	Customer_Id int foreign key References Customers(Id) ,
 );
 
 
-ALTER TABLE [Products] ADD CONSTRAINT [Products_fk3] FOREIGN KEY ([Category_Id]) REFERENCES [Categories]([id]);
 
-ALTER TABLE [Orders] ADD CONSTRAINT [Orders_fk3] FOREIGN KEY ([Customer_Id]) REFERENCES [Customers]([id]);
+CREATE TABLE Categories (
+	Id int primary key IDENTITY,
+Name nvarchar(30) NOT NULL,
+Description nvarchar(30) NOT NULL,	
+);
 
-ALTER TABLE [OrderItems] ADD CONSTRAINT [OrderItems_fk3] FOREIGN KEY ([Orders_Id]) REFERENCES [Orders]([id]);
+CREATE TABLE Products (
+Id int primary key IDENTITY,
+Name nvarchar(30) NOT NULL,
+Description nvarchar(30) NOT NULL,
+Price decimal  ,
+Category_Id int foreign key References Categories(Id) ,
+);
 
-ALTER TABLE [OrderItems] ADD CONSTRAINT [OrderItems_fk4] FOREIGN KEY ([Products_Id]) REFERENCES [Products]([id]);
-ALTER TABLE [ProductSuppliers] ADD CONSTRAINT [ProductSuppliers_fk1] FOREIGN KEY ([Products_Id]) REFERENCES [Products]([id]);
+CREATE TABLE Suppliers (
+Id int primary key IDENTITY,
+Name nvarchar(30) NOT NULL,
+ContactInfo nvarchar(30) NOT NULL,
+);
 
-ALTER TABLE [ProductSuppliers] ADD CONSTRAINT [ProductSuppliers_fk2] FOREIGN KEY ([Suplier_Id]) REFERENCES [Suppliers]([id]);
+CREATE TABLE OrderItems (
+    Id INT PRIMARY KEY IDENTITY,
+    Quantity INT NOT NULL,
+    UnitPriceQuantity INT NOT NULL,
+	OrdersId int foreign key References Orders(Id) ,
+	ProductId int foreign key References Products(Id) 
+);
+
+
+CREATE TABLE ProductSuppliers (
+	Id int primary key IDENTITY,
+	Products_Id int foreign key References Products(Id),
+	Suplier_Id int foreign key References Suppliers(Id) ,
+
+);
+
